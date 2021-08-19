78b5931caf70af4345c3e5b6b5997658baf39c6b
MAlexGG <alexandragalarzag@gmail.com>
2021-08-19 13:00:12 +0200

    delete cantara.js

--- scripts/centara.js
+++
@@ -1,8 +0,0 @@
-module.exports = async(page, website) => {
-    const { selectors } = website;
-    await page.goto(website.url);
-    await page.waitForSelector(selectors.infoContainer);
-    const closePopUp = await page.$(selectors.infoPopUp);
-    await closePopUp.evaluate(closePopUp => closePopUp.click());
-    alert('if you want to scrape the results you need to write your code first');
-};
